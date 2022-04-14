bfe54e69c9d84c7c27a2fd870056c76c3c64206c
lesley.ferreira <lesley.ferreira@thoughtworks.com>
2022-04-14 13:01:00 -0300

    @lesleydsilva - fix

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import os
 
 # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 JWT_SECRET_KEY = "dffs@#%FfdsfsdQWQ$"
-DATABASE_URL = "postgres://padvgpdcqxnhhz:4bbc774dc17de1779d7b30209454e703174989e85a033c606672b690c4bcab47@ec2-44-199 \
-               -143-43.compute-1.amazonaws.com:5432/d1evmfqifcmdk6 "
+DATABASE_URL = "postgres://edmmwpgcpbetqw:f3f8bd00950e4122302b9ae01254a5f108c964a82ebc9575ff23944da2b47f47@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d6oqhf9qt4llia"
 # PASSWORD = os.environ.get('PASSWORD')
 # DATABASE = os.environ.get('DATABASE')
 # USER = os.environ.get('USER')
