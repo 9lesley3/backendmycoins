a21fe677c4084de2ba01e4d509aef0463803295a
lesley.ferreira <lesley.ferreira@thoughtworks.com>
2022-04-14 12:27:26 -0300

    @lesleydsilva - Add Procfile

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+JWT_SECRET_KEY = "dffs@#%FfdsfsdQWQ$"
 DATABASE_URL = "postgres://padvgpdcqxnhhz:4bbc774dc17de1779d7b30209454e703174989e85a033c606672b690c4bcab47@ec2-44-199 \
                -143-43.compute-1.amazonaws.com:5432/d1evmfqifcmdk6 "
-PASSWORD = os.environ.get('PASSWORD')
-DATABASE = os.environ.get('DATABASE')
-USER = os.environ.get('USER')
-HOST = 'localhost'
-PORT = '5432'
+# PASSWORD = os.environ.get('PASSWORD')
+# DATABASE = os.environ.get('DATABASE')
+# USER = os.environ.get('USER')
+# HOST = 'localhost'
+# PORT = '5432'
 
